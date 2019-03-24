5daa410e4524eca492bb2c16e11a603f1049aa42
nonkr <nonkr@hotmail.com>
2019-03-24 21:38:30 +0800

    add stm32f103c8-minimal board

---
+++ bsp/stm32/stm32f103c8-minimal/applications/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+//#include "samples/oled_sample.h"
+
+int main(void)
+{
+//    oled_sample();
+
+    while (1)
+    {
+        rt_thread_mdelay(500);
+    }
+
+    return RT_EOK;
+}
---
+++ bsp/stm32/stm32f103c8-minimal/applications/samples/oled_sample.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under GPL, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2019-03-24 21:01
+ *
+ */
+
+#ifndef __OLED_SAMPLE_H__
+#define __OLED_SAMPLE_H__
+
+void oled_sample(void);
+
+#endif //__OLED_SAMPLE_H__
